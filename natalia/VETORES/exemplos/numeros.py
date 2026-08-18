cad9d937c053c17178188796049b4ebbe995510a
nates2912 <srakawaiiuwu@gmail.com>
2026-08-18 16:27:54 -0300

    vetores: ate o remove

---
+++ natalia/VETORES/exemplos/numeros.py
@@ -0,0 +1,25 @@
+numeros = [23,26,21,23,25]
+nomes = ["Mav", 'Gideon','Paule', 'Zhadie', 'Ada']
+
+#append - Insere um novo valor no vetor, sempre na última posição.
+nomes.append('Sally')
+print(nomes)
+
+#insert - Insere um novo valor na posiçao desejada.
+numeros.insert(2, 29)
+print(numeros)
+
+#pop - Deleta um valor pela sua posição - se ficar vazio, ele remove o ultimo.
+numeros.pop()
+print(numeros)
+
+#remove - Deleta um valor pelo seu conteúdo.
+nomes.remove('Ada')
+print(nomes)
+
+#sort
+#reverse
+#len
+#count
+#sum()
+#max - #min
